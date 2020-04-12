2befeb31d902360f49af0c56b8951f1f39f8ff2e
Saif Al Falah <saifalfalah@gmail.com>
2020-04-12 20:16:01 +0530

    fixing error

--- src/http/get-foo/foo.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async function http(request) {
-  return {
-    statusCode: 201,
-    headers: { "Content-Type": "application/json; charset=utf8" },
-    body: JSON.stringify({ ok: true }),
-  };
-};
