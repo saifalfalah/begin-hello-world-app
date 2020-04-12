51d32c2eccc00a563df36cfc37aea0a53142f7be
Saif Al Falah <saifalfalah@gmail.com>
2020-04-12 21:05:39 +0530

    adding error handling

--- src/http/get-foo/index.js
+++ src/http/get-foo/index.js
@@ -1,13 +1,21 @@
 exports.handler = async function http(req) {
-  console.log("Begin API called");
-  return {
-    headers: {
-      "content-type": "application/json; charset=utf8",
-      "cache-control":
-        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
-    },
-    body: JSON.stringify({
-      message: "Hello from your Begin API!",
-    }),
-  };
+  try {
+    console.log("Begin API called");
+    return {
+      headers: {
+        "content-type": "application/json; charset=utf8",
+        "cache-control":
+          "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+      },
+      body: JSON.stringify({
+        message: "Hello from your Begin API!",
+      }),
+    };
+  } catch (error) {
+    return {
+      body: JSON.stringify({
+        message: "An error ocurred. Sorry",
+      }),
+    };
+  }
 };
